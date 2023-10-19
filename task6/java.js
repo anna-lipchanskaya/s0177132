a5e6313fca72610838fb9f69535a4e0ebdde8f4d
anna-lipchanskaya <144255662+anna-lipchanskaya@users.noreply.github.com>
2023-10-19 11:22:27 +0300

    Update java.js

--- task6/java.js
+++ task6/java.js
@@ -1,52 +1,117 @@
-
-  function click1() {
-    let f1 = document.getElementsByName("field1");
-    let f2 = document.getElementsByName("select1");
-    let r = document.getElementById("result");
-    if(f2[0].value == "v1") r.innerHTML = f1[0].value * 180;
-    if(f2[0].value == "v2") r.innerHTML = f1[0].value * 100;
-    if(f2[0].value == "v3") r.innerHTML = f1[0].value * 1800;
-    if(f2[0].value == "v4") r.innerHTML = f1[0].value * 60;
-    let s = document.getElementsByName("select1");
-    console.log(s[0].value);
-    return false;
+function isItNumber(str) {
+  return /^-?\d+([.,](\d)+)?$/.test(str);
+}
+function calculate() {
+ 
+  let quantity_of_goods = document.getElementById("quantity_of_goods");
+  let goods_type = document.getElementById("goods_type");
+  let result_field = document.getElementById("result");
+ 
+  let aubergine_options = document.getElementById("aubergine_options");
+  let loan_options = document.getElementById("loan_options");
+  let loan_options_type = document.getElementsByName("loan_options_type");
+ 
+  let result = 0;
+  let aubergine_options_result = 0;
+  let loan_options_result = 0;
+ 
+  if (quantity_of_goods.value === "") {
+    result_field.innerHTML = "0";
+    return;
   }
-
-window.addEventListener('DOMContentLoaded', function (event) {
-  let s = document.getElementsByName("myradios");
-  s[0].addEventListener("change", function(event) {
-    let radios = event.target;
-    let select1 = document.getElementById("myselect1");
-    let select2 = document.getElementById("myselect2");
-
-    if (radios.value == "r2") {
-      select1.style.display = "block";
+ 
+  if (!isItNumber(quantity_of_goods.value)){
+    result_field.innerHTML = "0";
+    return;
+  }
+  else {
+    quantity_of_goods.value = quantity_of_goods.value.replace(',', '.');
+  }
+ 
+  if (quantity_of_goods.value < 0) {
+    result_field.innerHTML = "Enter a number greater than or equal to 0";
+    return;
+  }
+ 
+  let loan_prices = new Map ([
+    ["с1", 300],
+    ["с2", 1000]
+  ]);
+ 
+  let aubergine_freshness = new Map ([
+    ["s1", 500],
+    ["s2", 1000],
+    ["s3", 1500]
+  ]);
+ 
+  if (aubergine_options.style.display == "block")
+      aubergine_options_result += aubergine_freshness.get(aubergine_options.value)
+  else
+    aubergine_options_result = 0
+ 
+  if (loan_options.style.display == "block"){
+    for (i = 0; i < loan_options_type.length; i++){
+      if (loan_options_type[i].checked)
+        loan_options_result += loan_prices.get(loan_options_type[i].value)
     }
-    else {
-      select1.style.display = "none";
+  }
+  else
+    loan_options_result = 0
+ 
+  let goods = new Map([
+    ["v1", 180],
+    ["v2", 100],
+    ["v3", 1800],
+    ["v4", 60]
+  ]);
+ 
+  result += parseFloat(quantity_of_goods.value) * goods.get(goods_type.value) + aubergine_options_result + loan_options_result;
+ 
+  result_field.innerHTML = (result).toString();
+}
+ 
+function showDependOnRadioButton(){
+  let radio_button_service_types = document.getElementsByName("service_type");
+  let aubergine_options = document.getElementById("aubergine_options");
+  let loan_options = document.getElementById("loan_options");
+ 
+  for (i = 0; i < radio_button_service_types.length; i++){
+ 
+    if (radio_button_service_types[i].checked){
+ 
+      if (radio_button_service_types[i].value == "Flowers"){
+        aubergine_options.style.display = "none";
+        loan_options.style.display = "none";
+      }
+      else if (radio_button_service_types[i].value == "Loans"){
+        loan_options.style.display = "block";
+        aubergine_options.style.display = "none"
+      }
+      else {
+        aubergine_options.style.display = "block"
+        loan_options.style.display = "none"
+      }
     }
-
-    if (radios.value == "r3") {
-      select2.style.display = "block";
+  }
+}
+ 
+window.addEventListener(
+  'DOMContentLoaded',
+  function () {
+            let b = document.getElementById("calculate_button");
+            b.addEventListener("click", calculate);
+          }
+);
+ 
+let radio_default = document.getElementById("radio_button_checked_by_default")
+radio_default.checked = true
+ 
+window.addEventListener(
+  'DOMContentLoaded',
+  function () {
+    let radio_button_service_types = document.getElementsByName("service_type");
+    for(i = 0; i < radio_button_service_types.length; i++){
+      radio_button_service_types[i].addEventListener("change", showDependOnRadioButton)
     }
-    else {
-      select2.style.display = "none";
-    }
-  });
-  
-  let r1 = document.querySelectorAll(".myselect1 input");
-  r1.forEach(function(select1) {
-    select1.addEventListener("change", function(event) {
-      let r1 = event.target;
-      console.log(r1.value);
-    });    
-  });
-  
-  let r2 = document.querySelectorAll(".myselect2 input[type = checkbox]");
-  r2.forEach(function(select2) {
-    select2.addEventListener("change", function(event) {
-      let r2 = event.target;
-      console.log(r2.value);
-    });    
-  });
-});
+  }
+)
